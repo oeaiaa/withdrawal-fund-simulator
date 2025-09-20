d8a28b2326bd28c9f5ae322067403957e3e2230a
oeaiaa <122397482+oeaiaa@users.noreply.github.com>
2025-09-20 21:42:05 +0300

    Change base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/withdrawal-simulator/', // Replace 'withdrawal-simulator' with your actual repository name
+  base: '/withdrawal-fund-simulator/', // Replace 'withdrawal-simulator' with your actual repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
